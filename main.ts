b72032ae7a937bf0f44b145a83052a4a090ce1b7
mrlilholt <63080594+mrlilholt@users.noreply.github.com>
2021-04-20 07:58:41 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+/**
+ * At anytime, you can right-click on a block and select "help" for more info on the specific block and how it can be used.
+ */
+// This is your on-start block.  This container of code will run when you start your game.
+// 
